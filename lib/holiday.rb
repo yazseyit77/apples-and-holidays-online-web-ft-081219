4962d98f7a6a09355c7eb4f1a0173348bab89318
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:38:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+    holiday_hash[season][holiday_name] = supply_array
   # remember to return the updated hash
 
 end
