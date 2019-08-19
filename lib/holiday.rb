f26cb54a87283deef7121e8d064502ca6676248d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:39:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].include?(suppy)
 end
 
 def all_supplies_in_holidays(holiday_hash)
