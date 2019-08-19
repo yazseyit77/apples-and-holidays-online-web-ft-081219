8b53b8bb974dc22a6a1ce953a9429bb389bb6ad7
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:47:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,7 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   #   }
   # }
   # return the second element in the 4th of July array
-    holiday_supplies[:spring][:fourth_of_july][1]
+    holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
