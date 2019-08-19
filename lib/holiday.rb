6360711977bd98b6dc43c0d21b721e34e5b879d1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 06:06:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,9 +70,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map do |attribute, value|
-    value.map do |value, item|
+
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, item|
       holiday if item.include?("BBQ")
-    end
+    end 
   end.flatten.compact
+
 end
