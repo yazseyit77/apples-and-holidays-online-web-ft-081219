20d966042002b26c7a33e42305dc96f279577c0d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:56:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |attribute, value|
     puts "#{attribute.capitalize}:"
     value.each do |value, item|
-      
+
     end
   end
 end
