af2d3062d44cf94a3e8cc0718c0bf75e76ff7d85
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:53:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |attribute, value|
-    
-  end  
+
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
