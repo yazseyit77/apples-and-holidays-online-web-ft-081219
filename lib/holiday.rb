a765c272f931b2a46c45f6ef7e0855077ba6dfed
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 05:14:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     end
   end
 end
+second_supply_for_fourth_of_july(holiday_hash)
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
